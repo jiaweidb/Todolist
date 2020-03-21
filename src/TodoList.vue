883390f9e0c2045a0e5fd5d9d5b047102a49554d
jiaweidb <isjiawei@163.com>
2020-03-21 21:01:21 +0800

    第一次的todolist

---
+++ src/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>简易Todolist</h1>
+    <input v-model="inputValue"/>
+    <button v-on:click = "handleSubmit">提交</button>
+    <ul>
+      <todo-item  
+          v-for="(item, index) of list" 
+          :key="index"
+          :content="item"
+          :index="index"
+          @delete="handleDelete"
+          >
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+/*使用小组件 */
+import TodoItem from './components/TodoItem'
+
+
+export default {
+  /*将局部组件绑定到该标签之上*/
+  components: {
+    'todo-item':TodoItem
+  },
+  data: function () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue);
+      this.inputValue = '';
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="item" v-on:click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content', 'index'],
+  methods:{
+    handleDelete(){
+      this.$emit('delete',this.index)
+    }
+  }
+ 
+}
+</script>
+
+
+<style scoped>
+.item{
+  color:green;
+}
+
+</style>
